081e0b91305b4ab28a356b8f49b58cb045113616
Lucas de Pádua <72556348+Padualb@users.noreply.github.com>
2022-10-20 16:39:24 -0300

    Model creation

--- place/models.py
+++ place/models.py
@@ -1,5 +1,26 @@
+from distutils.command.upload import upload
+from email.policy import default
+from unittest.util import _MAX_LENGTH
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+CATEGORY_LIST = (
+    ("RESTAURANT", "Restaurant"),
+    ("ATTRACTIONS", "Attractions"),
+    ("PARTYS", "Partys"),
+    ("OTHER", "Other")
+)
 
+class Place(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=1000)
+    thumb = models.ImageField(upload_to='thumb-places')
+    category = models.CharField(max_length=15, choices=CATEGORY_LIST)
+    adress = models.CharField(max_length=100)
+    creation_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
 
