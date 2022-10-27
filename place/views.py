c5022d7f5409cea94fc123c07e23f0d46f440f95
Lucas de Pádua <72556348+Padualb@users.noreply.github.com>
2022-10-27 10:04:45 -0300

    Mudanças HTML

--- place/views.py
+++ place/views.py
@@ -13,6 +13,22 @@ class HomePlaces(ListView):
     template_name = "homeplaces.html"
     model = Place
 
+   
 class DetailsPlace(DetailView):
     template_name = "detailsplace.html"
     model = Place
+
+    # def get(self, request, *args, **kwargs):
+    #     place = self.get_object()
+    #     average_rating = 
+        
+        
+    #     return super().get(request, *args, **kwargs)
+
+
+    def get_context_data(self, **kwargs):
+        context = super(DetailsPlace,self).get_context_data(**kwargs)
+        related_places = Place.objects.filter(category=self.get_object().category)
+        context["related_places"] = related_places 
+        return context
+
--- visitBrasilia/settings.py
+++ visitBrasilia/settings.py
@@ -63,6 +63,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'place.new_context.list_recent_places',
+                'place.new_context.list_categorys',
             ],
         },
     },
