d90beaff2186f4946b28ebe336a50bbd30e31140
Lucas de Pádua <72556348+Padualb@users.noreply.github.com>
2022-11-01 14:16:56 -0300

    Reformulação Homepage

--- place/new_context.py
+++ place/new_context.py
@@ -12,3 +12,18 @@ def list_categorys(request):
     list_categorys = ("Restaurants", "Attractions", "Parties", "Other")
     return{"list_categorys": list_categorys}
 
+def list_attractions(request):
+    list_attractions = Place.objects.filter(category="ATTRACTIONS")
+    return{"list_attractions": list_attractions}
+
+def list_parties(request):
+    list_parties = Place.objects.filter(category="PARTIES")
+    return{"list_parties": list_parties}
+
+def list_restaurants(request):
+    list_restaurants= Place.objects.filter(category="RESTAURANTS")
+    return{"list_restaurants": list_restaurants}
+
+def list_others(request):
+    list_others = Place.objects.filter(category="OTHER")
+    return{"list_others": list_others}       
